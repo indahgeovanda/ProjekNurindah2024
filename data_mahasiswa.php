c7f06efb546bc1a2bd5d50ad8d439d781f8329a2
indahgeovanda <159239656+indahgeovanda@users.noreply.github.com>
2024-02-25 12:55:34 +0700

    Update data_mahasiswa.php

--- data_mahasiswa.php
+++ data_mahasiswa.php
@@ -2,6 +2,8 @@
 echo"<h1>Data Mahasiswa </h1>";
 echo"NIM  : 22110016";
 echo"Nama  : Nurindah SR";
-echo"Kelas : 22 MI-A"
+echo"Kelas : 22 MI-A";
+echo"Semester :genap";
+echo"Tahun Akademik 2023/2024";
   ?>
   
